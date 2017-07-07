7a50db4c1b046973dd461dc79064f719acb07494
Łukasz Adamczak <lukasz@czak.pl>
2017-07-07 12:06:45 +0200

    Implement cleanup function

--- floppy.cpp
+++ floppy.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <SDL.h>
 
+#include "cleanup.h"
+
 int main(int, char**) {
   if (SDL_Init(SDL_INIT_VIDEO) != 0) {
     std::cout << "SDL_Init Error: " << SDL_GetError() << "\n";
@@ -16,7 +18,7 @@ int main(int, char**) {
 
   SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
   if (ren == nullptr) {
-    SDL_DestroyWindow(win);
+    cleanup(win);
     std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << "\n";
     SDL_Quit();
     return 1;
@@ -25,33 +27,29 @@ int main(int, char**) {
   std::string imagePath = "hello.bmp";
   SDL_Surface *bmp = SDL_LoadBMP(imagePath.c_str());
   if (bmp == nullptr) {
-    SDL_DestroyRenderer(ren);
-    SDL_DestroyWindow(win);
+    cleanup(ren, win);
     std::cout << "SDL_LoadBMP Error: " << SDL_GetError() << "\n";
     SDL_Quit();
     return 1;
   }
 
   SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, bmp);
-  SDL_FreeSurface(bmp);
+  cleanup(bmp);
   if (tex == nullptr) {
-    SDL_DestroyRenderer(ren);
-    SDL_DestroyWindow(win);
+    cleanup(ren, win);
     std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << "\n";
     SDL_Quit();
     return 1;
   }
 
-  for (int i = 0; i < 3; ++i) {
+  for (int i = 0; i < 1; ++i) {
     SDL_RenderClear(ren);
     SDL_RenderCopy(ren, tex, NULL, NULL);
     SDL_RenderPresent(ren);
     SDL_Delay(1000);
   }
 
-  SDL_DestroyTexture(tex);
-  SDL_DestroyRenderer(ren);
-  SDL_DestroyWindow(win);
+  cleanup(tex, ren, win);
   SDL_Quit();
   return 0;
 }
